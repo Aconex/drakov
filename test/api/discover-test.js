2363cd510ea4221caaea39b95e57a46143913bc1
Yakov Khalinsky <ykhalinsky@aconex.com>
2015-07-27 20:05:58 +1000

    Update discovery mount point to be /drakov

--- test/api/discover-test.js
+++ test/api/discover-test.js
@@ -12,10 +12,10 @@ describe('Discover', function() {
                 helper.drakov.stop(done);
             });
 
-            describe('/', function() {
+            describe('/drakov', function() {
                 describe('GET', function() {
                     it('should get discover page', function(done) {
-                        request.get('/')
+                        request.get('/drakov')
                             .expect(200)
                             .expect('Content-type', 'text/html; charset=utf-8')
                             .end(helper.endCb(done));
@@ -32,10 +32,10 @@ describe('Discover', function() {
                 helper.drakov.stop(done);
             });
 
-            describe('/', function() {
+            describe('/drakov', function() {
                 describe('GET', function() {
                     it('should get 404', function(done) {
-                        request.get('/')
+                        request.get('/drakov')
                             .expect(404)
                             .end(helper.endCb(done));
                     });
@@ -44,49 +44,4 @@ describe('Discover', function() {
         });
     });
 
-    describe('Root-Level-API', function() {
-        describe('Discover option not specified', function() {
-            before(function(done) {
-                helper.drakov.run({sourceFiles: 'test/example/md/root-level-api.md'}, done);
-            });
-
-            after(function(done) {
-                helper.drakov.stop(done);
-            });
-
-            describe('/', function() {
-                describe('GET', function() {
-                    it('should get root level request rather than discover page', function(done) {
-                        request.get('/')
-                            .expect(200)
-                            .expect('Content-type', 'application/json;charset=UTF-8')
-                            .expect({Slash: 'http://images5.fanpop.com/image/photos/31200000/Slash-slash-31278382-1707-2560.jpg'})
-                            .end(helper.endCb(done));
-                    });
-                });
-            });
-        });
-
-        describe('Discover option true', function() {
-            before(function(done) {
-                helper.drakov.run({sourceFiles: 'test/example/md/root-level-api.md', discover: true}, done);
-            });
-
-            after(function(done) {
-                helper.drakov.stop(done);
-            });
-
-            describe('/', function() {
-                describe('GET', function() {
-                    it('should get root level request rather than discover page', function(done) {
-                        request.get('/')
-                            .expect(200)
-                            .expect('Content-type', 'application/json;charset=UTF-8')
-                            .expect({Slash: 'http://images5.fanpop.com/image/photos/31200000/Slash-slash-31278382-1707-2560.jpg'})
-                            .end(helper.endCb(done));
-                    });
-                });
-            });
-        });
-    });
 });
