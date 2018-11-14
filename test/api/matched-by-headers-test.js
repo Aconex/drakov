512b4c5698e6e8a604ddffa16e09218853f89f22
Paul <pweinger@fubo.tv>
2018-11-14 18:15:11 -0500

    Clarify method names and test descriptions

--- test/api/matched-by-headers-test.js
+++ test/api/matched-by-headers-test.js
@@ -12,29 +12,32 @@ describe('matches-by-headers', () => {
         helper.drakov.stop(done);
     });
 
-    describe('/demo', () => {
+    describe('given the request body matches a schema but does not equal the body in the spec', () => {
+        const validBodyThatIsNotExactMatchWithSpecBody = {
+            'question': 'Why?',
+            'choices': ['Why not?', 'BECAUSE!']
+        };
 
-        describe('when the request matches a schema', () => {
-            it('should have the `x-matched-by: matches-request-schema` header', (done) => {
+        describe('when validating the request', () => {
+            it('the request is matched by schema and the response should have the `x-matched-by: matches-request-schema` header', (done) => {
                 request.post('/demo')
-                .send({
-                    'question': 'Why?',
-                    'choices': ['Why not?', 'BECAUSE!']
-                })
+                .send(validBodyThatIsNotExactMatchWithSpecBody)
                 .expect(200)
                 .expect('X-Matched-By', 'matches-request-schema')
                 .end(helper.endCb(done));
             });
         });
+    });
 
-
-        describe('when the request matches a body', () => {
-            it('should have the `x-matched-by: matches-request-body` header', (done) => {
+    describe('given the request body is an exact match to body in the spec', () => {
+        const bodyThatMatchesSpecBodyExactly = {
+            'question': 'Why?',
+            'choices': ['who knows?', 'BECAUSE!']
+        };
+        describe('when validating the request', () => {
+            it('the request is matched by body and the response should have the `x-matched-by: matches-request-body` header', (done) => {
                 request.post('/demo')
-                .send({
-                    'question': 'Why?',
-                    'choices': ['who knows?', 'BECAUSE!']
-                })
+                .send(bodyThatMatchesSpecBodyExactly)
                 .expect(200)
                 .expect('X-Matched-By', 'matches-request-body')
                 .end(helper.endCb(done));
